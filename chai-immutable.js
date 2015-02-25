01bbae20e4ac296457680dc0d1d7962554342dee
Jérémie Astori <jeremie@astori.fr>
2015-02-25 09:02:28 -0500

    Overwrite least, most, above, below, within for the size property, and add all aliases to match the official length property

--- chai-immutable.js
+++ chai-immutable.js
@@ -28,4 +28,125 @@ module.exports = function (chai, utils) {
 
   Assertion.addChainableMethod('size', assertCollectionSize, chainCollectionSize);
   Assertion.addMethod('sizeOf', assertCollectionSize);
+
+  // size property
+
+  function assertCollectionSize_least(_super) {
+    return function (n) {
+      if (utils.flag(this, 'immutable.collection.size')) {
+        new Assertion(this._obj).instanceof(Collection);
+
+        var size = this._obj.size;
+        new Assertion(size).a('number');
+
+        this.assert(
+            size >= n
+          , "expected #{this} to have a size of at least #{exp} but got #{act}"
+          , "expected #{this} to not have a size of at least #{exp} but got #{act}"
+          , n
+          , size
+        );
+      } else {
+        _super.apply(this, arguments);
+      }
+    };
+  }
+
+  function assertCollectionSize_most(_super) {
+    return function (n) {
+      if (utils.flag(this, 'immutable.collection.size')) {
+        new Assertion(this._obj).instanceof(Collection);
+
+        var size = this._obj.size;
+        new Assertion(size).a('number');
+
+        this.assert(
+            size <= n
+          , "expected #{this} to have a size of at most #{exp} but got #{act}"
+          , "expected #{this} to not have a size of at most #{exp} but got #{act}"
+          , n
+          , size
+        );
+      } else {
+        _super.apply(this, arguments);
+      }
+    };
+  }
+
+  function assertCollectionSize_above(_super) {
+    return function (n) {
+      if (utils.flag(this, 'immutable.collection.size')) {
+        new Assertion(this._obj).instanceof(Collection);
+
+        var size = this._obj.size;
+        new Assertion(size).a('number');
+
+        this.assert(
+            size > n
+          , "expected #{this} to have a size above #{exp} but got #{act}"
+          , "expected #{this} to not have a size above #{exp} but got #{act}"
+          , n
+          , size
+        );
+      } else {
+        _super.apply(this, arguments);
+      }
+    };
+  }
+
+  function assertCollectionSize_below(_super) {
+    return function (n) {
+      if (utils.flag(this, 'immutable.collection.size')) {
+        new Assertion(this._obj).instanceof(Collection);
+
+        var size = this._obj.size;
+        new Assertion(size).a('number');
+
+        this.assert(
+            size < n
+          , "expected #{this} to have a size below #{exp} but got #{act}"
+          , "expected #{this} to not have a size below #{exp} but got #{act}"
+          , n
+          , size
+        );
+      } else {
+        _super.apply(this, arguments);
+      }
+    };
+  }
+
+  Assertion.overwriteMethod('least', assertCollectionSize_least);
+  Assertion.overwriteMethod('gte', assertCollectionSize_least);
+
+  Assertion.overwriteMethod('most', assertCollectionSize_most);
+  Assertion.overwriteMethod('lte', assertCollectionSize_most);
+
+  Assertion.overwriteMethod('above', assertCollectionSize_above);
+  Assertion.overwriteMethod('gt', assertCollectionSize_above);
+  Assertion.overwriteMethod('greaterThan', assertCollectionSize_above);
+
+  Assertion.overwriteMethod('below', assertCollectionSize_below);
+  Assertion.overwriteMethod('lt', assertCollectionSize_below);
+  Assertion.overwriteMethod('lessThan', assertCollectionSize_below);
+
+  Assertion.overwriteMethod('within', function (_super) {
+    return function (min, max) {
+      if (utils.flag(this, 'immutable.collection.size')) {
+        new Assertion(this._obj).instanceof(Collection);
+
+        var size = this._obj.size;
+        new Assertion(size).a('number');
+
+        this.assert(
+            min <= size && size <= max
+          , "expected #{this} to have a size within #{exp} but got #{act}"
+          , "expected #{this} to not have a size within #{exp} but got #{act}",
+          min + ".." + max,
+          size
+        );
+      } else {
+        _super.apply(this, arguments);
+      }
+    };
+  });
 };
--- test/test.js
+++ test/test.js
@@ -24,4 +24,70 @@ describe('chai-immutable', function () {
       expect(list3).to.not.have.sizeOf(42);
     })
   });
+
+  describe('size property', function () {
+    it('above should be true when given a good min size', function () {
+      expect(list3).to.have.size.above(2);
+    });
+
+    it('above should be false when given a bad min size', function () {
+      expect(list3).to.not.have.size.above(42);
+    });
+
+    it('aliases of above should also work', function () {
+      expect(list3).to.have.size.gt(2);
+      expect(list3).to.have.size.greaterThan(2);
+      expect(list3).to.not.have.size.gt(42);
+      expect(list3).to.not.have.size.greaterThan(42);
+    });
+
+    it('below should be true when given a good max size', function () {
+      expect(list3).to.have.size.below(42);
+    });
+
+    it('below should be false when given a bad max size', function () {
+      expect(list3).to.not.have.size.below(1);
+    });
+
+    it('aliases of below should also work', function () {
+      expect(list3).to.have.size.lt(4);
+      expect(list3).to.have.size.lessThan(4);
+      expect(list3).to.not.have.size.lt(1);
+      expect(list3).to.not.have.size.lessThan(1);
+    });
+
+    it('within should be true when given a good range', function () {
+      expect(list3).to.have.size.within(2, 42);
+    });
+
+    it('within should be false when given a bad range', function () {
+      expect(list3).to.not.have.size.within(10, 20);
+    });
+
+    it('least should be true when given a good min size', function () {
+      expect(list3).to.have.size.of.at.least(2);
+    });
+
+    it('least should be false when given a bad min size', function () {
+      expect(list3).to.not.have.size.of.at.least(42);
+    });
+
+    it('aliases of least should also work', function () {
+      expect(list3).to.have.size.gte(2);
+      expect(list3).to.not.have.size.gte(42);
+    });
+
+    it('most should be true when given a good max size', function () {
+      expect(list3).to.have.size.of.at.most(42);
+    });
+
+    it('most should be false when given a bad max size', function () {
+      expect(list3).to.not.have.size.of.at.most(2);
+    });
+
+    it('aliases of most should also work', function () {
+      expect(list3).to.have.size.lte(42);
+      expect(list3).to.not.have.size.lte(2);
+    });
+  });
 });
