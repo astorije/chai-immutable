c4b5b7b8c17c54763812b5a9cb13f2db54c298ad
Jérémie Astori <jeremie@astori.fr>
2015-02-25 08:57:12 -0500

    Assert on the object before adding the size assertion

--- chai-immutable.js
+++ chai-immutable.js
@@ -1,17 +1,22 @@
 'use strict';
 
+var Collection = require('immutable').Collection;
+
 module.exports = function (chai, utils) {
   var Assertion = chai.Assertion;
 
-  Assertion.addMethod('size', function (size) {
-    var obj = this._obj;
+  Assertion.addMethod('size', function (n) {
+    new Assertion(this._obj).instanceof(Collection);
+
+    var size = this._obj.size;
+    new Assertion(size).a('number');
 
     this.assert(
-        obj.size === size
-      , "expected #{this} to have size #{exp} but got #{act}"
+        size === n
+      , "expected #{this} to have have size #{exp} but got #{act}"
       , "expected #{this} to not have size #{act}"
-      , size // expected
-      , obj.size // actual
+      , n
+      , size
     );
   });
 };
